9b010efd37eca3194507c75a81185359e984871b
Maisa <maisa.milena@gmail.com>
2017-07-04 17:07:44 -0300

    Consumo de todos os dogs funcionando

--- DogCRUD/DogCRUD/ViewController.swift
+++ DogCRUD/DogCRUD/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
+    let urlInsert = "http://localhost:3000/dogs"
+    
+    // Array de dicionários
+    var dogs : [[String: AnyObject]] = [[String: AnyObject]]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Determinando o parâmetro passado na url
+        //let parameters:Parameters = ["name": "dog do xcode", "color":"blue"]
+        
+      selectAllDogs()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func selectAllDogs(){
+        Alamofire.request(urlInsert, method: .get).responseJSON { (response) in
+            
+            switch response.result {
+            case .success:
+                self.dogs = response.result.value as! [[String: AnyObject]]
+                print(self.dogs)
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+            
+        }
+    }
+    
 
 }
 
