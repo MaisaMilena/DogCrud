81742312b1c07c5efd7c5f182fe3f5740e4e07c2
Ítalo Ferreira Sjobom Nápoles <sjobom.italo@gmail.com>
2017-10-30 17:00:37 -0200

    Implementação de testes de Interface

---
+++ DogCRUD/DogCRUDUITests/TelaListagemUITests.swift
@@ -0,0 +1,47 @@
+//
+//  TelaListagemUITests.swift
+//  DogCRUDUITests
+//
+//  Created by Ítalo Sjobom Nápoles on 29/10/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+import XCTest
+
+class TelaListagemUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testSelecionarDog(){
+        
+        let app = XCUIApplication()
+        app.buttons["  Listar todos   "].tap()
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Doguino atualizado novamente"]/*[[".cells.staticTexts[\"Doguino atualizado novamente\"]",".staticTexts[\"Doguino atualizado novamente\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let dogcrudAtualizartableviewNavigationBar = app.navigationBars["DogCRUD.AtualizarTableView"]
+        dogcrudAtualizartableviewNavigationBar.buttons["Listar"].tap()
+        
+    }
+    
+}
